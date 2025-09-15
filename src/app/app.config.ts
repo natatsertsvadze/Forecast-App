9f4d4bc3c2cc676d60419620cdf692add55fe7a2
nata <nata.cercvadze1@gmail.com>
2025-09-15 17:34:06 +0400

    fixed button-error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
+import { APP_BASE_HREF } from '@angular/common'  
+import { routes } from './app.routes'
+import { provideClientHydration } from '@angular/platform-browser'
 
 export const appConfig: ApplicationConfig = {
-  providers: [ provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withInterceptorsFromDi())]
-};
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: APP_BASE_HREF, useValue: '/Forecast-App/' }
+  ]
+}
